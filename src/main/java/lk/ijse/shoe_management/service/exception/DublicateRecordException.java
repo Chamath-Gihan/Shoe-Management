bcbed25254d2acfa6e5bf77f9f6bc09dff47140b
Chamath-Gihan <spchandanipushpakumari@gmail.com>
2024-05-31 00:16:03 +0530

    Create DublicateRecordException

---
+++ src/main/java/lk/ijse/shoe_management/service/exception/DublicateRecordException.java
@@ -0,0 +1,8 @@
+package lk.ijse.shoe_management.service.exception;
+
+public class DublicateRecordException extends ServiceException{
+    
+    public DublicateRecordException(String message) {
+        super(message);
+    }
+}
--- src/main/java/lk/ijse/shoe_management/service/exception/ServiceException.java
+++ src/main/java/lk/ijse/shoe_management/service/exception/ServiceException.java
@@ -1,7 +1,7 @@
 package lk.ijse.shoe_management.service.exception;
 
 public class ServiceException extends RuntimeException{
-    
+
     public ServiceException(String message){
         super(message);
     }
