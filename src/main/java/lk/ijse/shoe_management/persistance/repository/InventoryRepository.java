bef3b099a5ae6cdcfe874bff0f6df29016f2ae08
Chamath-Gihan <spchandanipushpakumari@gmail.com>
2024-05-30 23:51:49 +0530

    Create SalesDetailsRepository

--- src/main/java/lk/ijse/shoe_management/persistance/repository/InventoryRepository.java
+++ src/main/java/lk/ijse/shoe_management/persistance/repository/InventoryRepository.java
@@ -8,6 +8,6 @@ public interface InventoryRepository extends JpaRepository<Inventory,String> {
     Boolean existsByItemCode(String id);
 
     Inventory findByItemCode(String id);
-    
+
     void deleteByItemCode(String id);
 }
---
+++ src/main/java/lk/ijse/shoe_management/persistance/repository/SalesDetailsRepository.java
@@ -0,0 +1,17 @@
+package lk.ijse.shoe_management.persistance.repository;
+
+import lk.ijse.shoe_management.persistance.entity.SalesDetails;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface SalesDetailsRepository extends JpaRepository<SalesDetails,String> {
+
+    Boolean existsBySalesOrderNo(String id);
+
+    List<SalesDetails> findAllBySalesOrderNo(String id);
+
+    void deleteAllBySalesOrderNo(String id);
+    
+    Boolean existsAllBySalesOrderNo(String id);
+}
