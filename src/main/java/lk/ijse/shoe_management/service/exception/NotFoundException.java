aed0b4bda47c556f0f647a2e7a261a78d6680618
Chamath-Gihan <spchandanipushpakumari@gmail.com>
2024-05-31 00:20:30 +0530

    Class CustomerServiceImpl

--- src/main/java/lk/ijse/shoe_management/service/exception/NotFoundException.java
+++ src/main/java/lk/ijse/shoe_management/service/exception/NotFoundException.java
@@ -1,7 +1,7 @@
 package lk.ijse.shoe_management.service.exception;
 
 public class NotFoundException extends ServiceException{
-    
+
     public NotFoundException(String message) {
         super(message);
     }
