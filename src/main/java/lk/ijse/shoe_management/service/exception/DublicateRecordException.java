e205dff56ebc448a0f3d1c31a518f4b43f0a7b49
Chamath-Gihan <spchandanipushpakumari@gmail.com>
2024-05-31 00:16:48 +0530

    Create NotFoundException

--- src/main/java/lk/ijse/shoe_management/service/exception/DublicateRecordException.java
+++ src/main/java/lk/ijse/shoe_management/service/exception/DublicateRecordException.java
@@ -1,7 +1,7 @@
 package lk.ijse.shoe_management.service.exception;
 
 public class DublicateRecordException extends ServiceException{
-    
+
     public DublicateRecordException(String message) {
         super(message);
     }
---
+++ src/main/java/lk/ijse/shoe_management/service/exception/NotFoundException.java
@@ -0,0 +1,8 @@
+package lk.ijse.shoe_management.service.exception;
+
+public class NotFoundException extends ServiceException{
+    
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
