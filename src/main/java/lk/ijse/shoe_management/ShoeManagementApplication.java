e54a0065c671f0fd6cb4f774527ad4c0f9612d8b
Chamath-Gihan <spchandanipushpakumari@gmail.com>
2024-05-30 22:43:27 +0530

    Add maven dependencies

---
+++ src/main/java/lk/ijse/shoe_management/ShoeManagementApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.shoe_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoeManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoeManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lk/ijse/shoe_management/ShoeManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.shoe_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoeManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
