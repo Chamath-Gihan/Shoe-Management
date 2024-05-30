6f00dd16ba8ec8ac83f0cd32efd46aa210d0fdc9
Chamath-Gihan <spchandanipushpakumari@gmail.com>
2024-05-30 23:55:45 +0530

    Create SupplierRepository

--- src/main/java/lk/ijse/shoe_management/persistance/repository/SecurityRepository.java
+++ src/main/java/lk/ijse/shoe_management/persistance/repository/SecurityRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface SecurityRepository extends JpaRepository<User,String> {
-    
+
     Optional<User> findByEmail(String email);
 }
---
+++ src/main/java/lk/ijse/shoe_management/persistance/repository/SupplierRepository.java
@@ -0,0 +1,18 @@
+package lk.ijse.shoe_management.persistance.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.function.Supplier;
+
+public interface SupplierRepository extends JpaRepository<Supplier,String> {
+
+    Boolean existsBySupplierCode(String id);
+
+    Supplier findBySupplierCode(String id);
+
+    void deleteBySupplierCode(String id);
+    
+    @Query(value = "SELECT supplier_code FROM Suppliers ORDER BY supplier_code DESC LIMIT 1", nativeQuery = true)
+    String findLatestSupplierCode();
+}
