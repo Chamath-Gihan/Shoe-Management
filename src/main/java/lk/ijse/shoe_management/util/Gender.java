ae41664cca85317380427834f4ab79c599af22a8
Chamath-Gihan <spchandanipushpakumari@gmail.com>
2024-05-30 22:48:27 +0530

    Create CustomerDTO and util.Level

---
+++ src/main/java/lk/ijse/shoe_management/util/Gender.java
@@ -0,0 +1,8 @@
+package lk.ijse.shoe_management.util;
+
+public enum Gender {
+
+    MALE,
+    FEMALE,
+    OTHER
+}
---
+++ src/main/java/lk/ijse/shoe_management/util/Level.java
@@ -0,0 +1,9 @@
+package lk.ijse.shoe_management.util;
+
+public enum Level {
+
+    GOLD,
+    SILVER,
+    BRONZE,
+    NEW
+}
