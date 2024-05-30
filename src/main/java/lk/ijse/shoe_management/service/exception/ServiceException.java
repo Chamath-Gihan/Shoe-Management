bd85007da8703018740a96fe7b73ee93f5251747
Chamath-Gihan <spchandanipushpakumari@gmail.com>
2024-05-31 00:15:18 +0530

    Create ServiceException

---
+++ src/main/java/lk/ijse/shoe_management/service/exception/ServiceException.java
@@ -0,0 +1,8 @@
+package lk.ijse.shoe_management.service.exception;
+
+public class ServiceException extends RuntimeException{
+    
+    public ServiceException(String message){
+        super(message);
+    }
+}
